d0ecf52a9d479d54a3dab40c0c3abcbdb77403d9
ponyspy <desade4me@gmail.com>
2013-09-20 12:54:19 +0400

    changes

--- public/scripts/static.js
+++ public/scripts/static.js
@@ -1,11 +1,25 @@
-$(document).ready(function() {
-	$('.description_block:first, .image:first').show();
+// $(document).ready(function() {
+// 	$('.description_block:first, .image:first').show();
 
-	$('.select_item').click(function() {
-		$('.image, .description_block').hide();
+// 	$('.select_item').click(function() {
+// 		var index = $(this).index();
+
+// 		$('.image, .description_block').hide();
+
+// 		$('.image').eq(index).show();
+// 		$('.description_block').eq(index).show();
+// 	});
+// });
+
+
+
+
+$(document).ready(function() {
+	$('.image :first').show();
+
+	$('.select_item').click(function(event) {
+		$('.image').hide();
 		var index = $(this).index();
-		alert(index)
-		$('.image').eq(index).show();
-		$('.description_block').eq(index).show();
+		$('.image').eq(index).show()
 	});
 });
