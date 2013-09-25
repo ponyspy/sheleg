a2eca647e391d1996b27a920fa188d836fab61de
ponyspy <desade4me@gmail.com>
2013-09-25 20:48:09 +0400

    animate fix

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
-	$('.main_menu_item').on({
-		mouseover: function(event) {
-			$(this).children('.main_menu_drop').slideDown('fast');
-			var color = $(this).children('.main_menu_drop').css('background-color')
-			$('.main_menu_block').css('border-color', color);
-		},
-		mouseout: function(event) {
-			$(this).children('.main_menu_drop').slideUp('fast');
-			$('.main_menu_block').css('border-color', 'black');
-		}
-	})
+	$('.main_menu_item').hover(function() {
+		$(this).children('.main_menu_drop').slideDown('fast');
+		var color = $(this).children('.main_menu_drop').css('background-color')
+		$('.main_menu_block').css('border-color', color);
+	}, function() {
+		$(this).children('.main_menu_drop').slideUp('fast');
+		$('.main_menu_block').css('border-color', 'black');
+	});
 });
