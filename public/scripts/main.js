dbe0dc7298098411235e81083a49f0ad123cfda9
ponyspy <desade4me@gmail.com>
2013-09-25 19:10:45 +0400

    cool changes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,9 +2,12 @@ $(document).ready(function() {
 	$('.main_menu_item').on({
 		mouseover: function(event) {
 			$(this).children('.main_menu_drop').show();
+			var color = $(this).children('.main_menu_drop').css('background-color')
+			$('.main_menu_block').css('border-color', color);
 		},
 		mouseout: function(event) {
 			$(this).children('.main_menu_drop').hide();
+			$('.main_menu_block').css('border-color', 'black');
 		}
 	})
 });
--- public/scripts/static.js
+++ public/scripts/static.js
@@ -1,25 +1,10 @@
-// $(document).ready(function() {
-// 	$('.description_block:first, .image:first').show();
-
-// 	$('.select_item').click(function() {
-// 		var index = $(this).index();
-
-// 		$('.image, .description_block').hide();
-
-// 		$('.image').eq(index).show();
-// 		$('.description_block').eq(index).show();
-// 	});
-// });
-
-
-
-
 $(document).ready(function() {
-	$('.image :first').show();
+	$('.service_block:first').show();
 
-	$('.select_item').click(function(event) {
-		$('.image').hide();
+	$('.select_item').click(function() {
 		var index = $(this).index();
-		$('.image').eq(index).show()
+
+		$('.service_block').hide();
+		$('.service_block').eq(index).show();
 	});
 });
