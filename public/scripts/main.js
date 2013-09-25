1d2e6ec84883b064f28e02e0a0917ef9343e2bc1
ponyspy <desade4me@gmail.com>
2013-09-25 23:51:18 +0400

    styles

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
 	$('.main_menu_item').hover(function() {
-		$(this).children('.main_menu_drop').slideDown('fast');
+		$(this).children('.main_menu_drop').stop(true, true).slideDown('fast');
 		var color = $(this).children('.main_menu_drop').css('background-color')
 		$('.main_menu_block').css('border-color', color);
 	}, function() {
-		$(this).children('.main_menu_drop').slideUp('fast');
-		$('.main_menu_block').css('border-color', 'black');
+		$(this).children('.main_menu_drop').stop(true, true).slideUp('fast').promise().done(function() {
+			$('.main_menu_block').css('border-color', 'black');
+		});
 	});
 });
