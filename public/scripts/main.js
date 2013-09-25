fdf214e39a15fd39dd82fa3e413c7b2e7279c64b
ponyspy <desade4me@gmail.com>
2013-09-25 19:38:33 +0400

    changes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 	$('.main_menu_item').on({
 		mouseover: function(event) {
-			$(this).children('.main_menu_drop').show();
+			$(this).children('.main_menu_drop').slideDown('fast');
 			var color = $(this).children('.main_menu_drop').css('background-color')
 			$('.main_menu_block').css('border-color', color);
 		},
 		mouseout: function(event) {
-			$(this).children('.main_menu_drop').hide();
+			$(this).children('.main_menu_drop').slideUp('fast');
 			$('.main_menu_block').css('border-color', 'black');
 		}
 	})
